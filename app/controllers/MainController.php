a3fc0fcdd847d519da8150afb0812a806dd772e7
unknown <dnvcomp@hotmail.com>
2019-04-15 17:38:31 +0300

    2.3 Database, 2.4 Branding

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -14,6 +14,8 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+        $brands = \R::find('brand', 'LIMIT 3');
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands'));
     }
 }
